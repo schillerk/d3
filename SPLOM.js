7009f3caffa75272c9ff1da078b863fb8d835025
Sam Neubauer <samneub@gmail.com>
2017-04-24 17:27:16 -0500

    Added all scatter plots to a single svg

--- SPLOM.js
+++ SPLOM.js
@@ -11,8 +11,13 @@ xOffset = 40;		// Space for x-axis labels
 yOffset = 40;		// Space for y-axis labels
 margin = 0;		// Margin around visualization
 vals = ['flight_index','num_o_ring_distress','launch_temp','leak_check_pressure','tufte_metric'];
+axis_names = {'flight_index': 'Flight Index', 
+			  'num_o_ring_distress': 'O-rings Experiencing Distress',
+			  'launch_temp': 'Launch Temperature (degrees F)',
+			  'leak_check_pressure': 'Leak-check Pressure (psi)',
+			  'tufte_metric': 'Tufte Metric',};
 
-function plot(xIndex, yIndex) {
+function plot(xIndex, yIndex, svg) {
 	var xVal = vals[xIndex];
 	var yVal = vals[yIndex];
 	// console.log(xVal);
@@ -21,21 +26,19 @@ function plot(xIndex, yIndex) {
 	d3.csv('challenger.csv', function(csvData) {
 		data = csvData;
 
+		var xBase = w*xIndex;
+		var yBase = h*yIndex;
+
 		// This will define scales that convert values
 		// from our data domain into screen coordinates.
 		xScale = d3.scale.linear()
 					.domain([d3.min(data, function(d) { return parseFloat(d[xVal]); })-1,
 							 d3.max(data, function(d) { return parseFloat(d[xVal]); })+1])
-						.range([yOffset + margin, w - margin]);
+						.range([yOffset + margin + xBase, w - margin + xBase]);
 		yScale = d3.scale.linear()
 					.domain([d3.min(data, function(d) { return parseFloat(d[yVal]); })-1,
 							 d3.max(data, function(d) { return parseFloat(d[yVal]); })+1])
-					.range([h - xOffset - margin, margin]); // Notice this is backwards!
-
-		// Next, we will create an SVG element to contain our visualization.
-		svg = d3.select('#pointsSVG').append('svg:svg')
-					.attr('width', w)
-					.attr('height', h);
+					.range([h - xOffset - margin + yBase, margin + yBase]); // Notice this is backwards!
 
 		// Build axes! (These are kind of annoying, actually...)
 		xAxis = d3.svg.axis()
@@ -44,12 +47,12 @@ function plot(xIndex, yIndex) {
 					.ticks(5);
 		xAxisG = svg.append('g')
 					.attr('class', 'axis')
-					.attr('transform', 'translate(0,' + (h - xOffset) + ')')
+					.attr('transform', 'translate(0,' + (h - xOffset + yBase) + ')')
 					.call(xAxis);
 		xLabel = svg.append('text')
 					.attr('class','label')
-					.attr('x', w/2)
-					.attr('y', h - 5)
+					.attr('x', w/2 + xBase)
+					.attr('y', h - 5 + yBase)
 					.text(xVal);
 		            // Uncomment the following event handler to change xVal by clicking label (and remove above semi-colon)
 					//.on('click', function() {
@@ -61,13 +64,15 @@ function plot(xIndex, yIndex) {
 					.ticks(5);
 		yAxisG = svg.append('g')
 					.attr('class', 'axis')
-					.attr('transform', 'translate(' + yOffset + ',0)')
+					.attr('transform', 'translate(' + (yOffset + xBase) + ',0)')
+					.attr('left', xBase)
+					.attr('position', 'absolute')
 					.call(yAxis);
 		yLabel = svg.append('text')
 					.attr('class','label')
 					.attr('transform', 'rotate(-90)')
-					.attr('x', -yOffset-40)
-					.attr('y', h/2-90) //TODO(SAM) make this cloud based
+					.attr('x', (-yOffset-40 - xBase))
+					.attr('y', (h/2-90 + yBase)) //TODO(SAM) make this cloud based
 					.text(yVal);
 					// Uncomment the following event handler to change yVal by clicking label (and remove above semi-colon)
 					//.on('click', function() {
@@ -90,40 +95,76 @@ function plot(xIndex, yIndex) {
 				// radius
 	            // color
 				// tooltip?
-		circles = svg.selectAll('circle')
-			.data(csvData);
-		circles.enter()
-			.append('svg:circle')
-			.attr('class', function(d) { return "x" + parseInt(d['flight_index']); })
-			.attr('cx', function(d) { return xScale(d[xVal]); })
-			.attr('cy', function(d) { return yScale(d[yVal]); })
-			.attr('r', 5)
-			.style("fill", "red");
 
-		circles
-			.append('svg:title')
-			.text(function(d) {return "flight index: " + d['flight_index']; });
-
-		circles
-			.on('mouseover', function() {
-				curPoint = d3.select(this);
-				d3.selectAll("." + curPoint.attr('class'))
-					.attr('r', 10);
-			})
-
-			.on('mouseout', function() {
-				curPoint = d3.select(this);
-				d3.selectAll("." + curPoint.attr('class'))
-					.attr('r', 5);
-			})
+		// circles = svg.selectAll('circle')
+		// 	.data(csvData);
+		// circles.enter()
+		// 	.append('svg:circle')
+		// 	.attr('class', function(d) { return "x" + parseInt(d['flight_index']); })
+		// 	.attr('cx', function(d) { return xScale(d[xVal]) + xBase; })
+		// 	.attr('cy', function(d) { return yScale(d[yVal]) + yBase; })
+		// 	.attr('r', 5)
+		// 	.style("fill", "red");
+		for (i = 1; i < 23; i++) {
+			circle = svg.append('circle')
+						.attr('class', "x" + parseInt(i))
+						.attr('cx', xScale(data[i][xVal]))
+						.attr('cy', yScale(data[i][yVal]))
+						.attr('r', 5)
+						.style("fill", "red");
 		
-			.on('click', function() {
-				curPoint = d3.select(this);
-				currentColor = curPoint.attr("style")
-				newColor = currentColor == "fill: red;" ? "blue" : "red";
-				d3.selectAll("." + curPoint.attr("class"))
-					.style("fill", newColor);
-			});
+			circle
+				.append('svg:title');
+				//.text(function(d) {return vals.map(val => "\n" + axis_names[val] + ": " + d[val]); } );
+
+			circle
+				.on('mouseover', function() {
+					curPoint = d3.select(this);
+					d3.selectAll("." + curPoint.attr('class'))
+						.attr('r', 10);
+				})
+
+				.on('mouseout', function() {
+					curPoint = d3.select(this);
+					d3.selectAll("." + curPoint.attr('class'))
+						.attr('r', 5);
+				})
+			
+				.on('click', function() {
+					curPoint = d3.select(this);
+					currentColor = curPoint.attr("style")
+					newColor = currentColor == "fill: red;" ? "blue" : "red";
+					d3.selectAll("." + curPoint.attr("class"))
+						.style("fill", newColor);
+				});
+		}
+
+		// circles = svg.selectAll('circle');
+		
+		// circles
+		// 	.append('svg:title');
+		// 	.text(function(d) {return vals.map(val => "\n" + axis_names[val] + ": " + d[val]); } );
+
+		// circles
+		// 	.on('mouseover', function() {
+		// 		curPoint = d3.select(this);
+		// 		d3.selectAll("." + curPoint.attr('class'))
+		// 			.attr('r', 10);
+		// 	})
+
+		// 	.on('mouseout', function() {
+		// 		curPoint = d3.select(this);
+		// 		d3.selectAll("." + curPoint.attr('class'))
+		// 			.attr('r', 5);
+		// 	})
+		
+		// 	.on('click', function() {
+		// 		curPoint = d3.select(this);
+		// 		currentColor = curPoint.attr("style")
+		// 		newColor = currentColor == "fill: red;" ? "blue" : "red";
+		// 		d3.selectAll("." + curPoint.attr("class"))
+		// 			.style("fill", newColor);
+		// 	});
 	});
 }
 
@@ -168,9 +209,13 @@ function setYval(val) {
 	// ************************************************
 }
 
+// Next, we will create an SVG element to contain our visualization.
+svg = d3.select('#pointsSVG').append('svg:svg')
+	.attr('width', w*5)
+	.attr('height', h*5);
 
 for(x = 0; x < 5; x++) {
 	for(y = 0; y < 5; y++) {
-		plot(x,y);				
+		plot(x, y, svg);				
 	}
 }
